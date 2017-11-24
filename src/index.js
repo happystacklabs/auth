cdc1b85af4c18eea89ddd7c01d2fd2abee88b883
Heinrich Tremblay <het083@g.harvard.edu>
2017-11-24 14:30:50 +0800

    installed redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </Router>
+  </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
