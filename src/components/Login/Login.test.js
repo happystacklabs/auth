fb09c68bd05aaed230579495cfc3992a4ac1deb8
Heinrich Tremblay <het083@g.harvard.edu>
2017-11-24 17:43:40 +0800

    basic Link test in Login

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './Login';
 import App from '../App/App';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Link } from 'react-router-dom';
 
 
 describe('Login', () => {
@@ -15,5 +15,14 @@ describe('Login', () => {
       );
       expect(app.containsMatchingElement(<Login/>)).toBe(true);
     });
+  });
+
+  describe('render create account Link', () => {
+    const app = mount(
+      <MemoryRouter>
+        <Login/>
+      </MemoryRouter>
+    );
+    expect(app.containsMatchingElement(<Link to='/register'>Create an account</Link>)).toBe(true);
   });
 });
